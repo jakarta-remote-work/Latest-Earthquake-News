744ef22f4e89e24f6e2f57f2aa5961e30b2f765f
firman <firmanap22@gmail.com>
2021-12-08 16:13:02 +0700

    release to pypi

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/jakarta-remote-work/Latest-Earthquake-News",
     project_urls={
-        "Bug Tracker": "https://github.com/jakarta-remote-work/Latest-Earthquake-News",
+
     },
     classifiers=[
         "Programming Language :: Python :: 3",
